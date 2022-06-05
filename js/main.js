4362d047480009459a9399bf386cb6674741da00
Wagner Paulo <wagstalos@hotmail.com>
2022-06-05 14:44:25 -0300

    js

--- js/main.js
+++ js/main.js
@@ -7,39 +7,39 @@ const btnMain = document.getElementById("js-btn-main");
 const iconCheck = document.getElementsByClassName("fa-check");
 const iconCopy = document.getElementsByClassName("fa-copy");
 
-function copyText(){
-    texto.select();
-    document.execCommand("copy");
-    phrases.innerText = 'Texto copiado: ' + texto.value;
-    btnCopyText.innerText = 'Texto copiado';
-    btnMain.style.background = "#1dd1a1";
-    iconCheck[0].style.display = "inline-block";
-    iconCopy[0].style.display = "none";
+function copyText() {
+  texto.select();
+  document.execCommand("copy");
+  phrases.innerText = "Texto copiado: " + texto.value;
+  btnCopyText.innerText = "Texto copiado";
+  btnMain.style.background = "#1dd1a1";
+  iconCheck[0].style.display = "inline-block";
+  iconCopy[0].style.display = "none";
 }
 
-function cleanText(){
-    texto.value = '';
-    phrases.innerHTML = '';
-    result.innerHTML = '';
-    result.classList.remove("backResult");
-    btnClean.style.display = "none";
-    btnCopyText.innerText = 'Copiar texto';
-    btnMain.style.background = "#e84393";
-    iconCheck[0].style.display = "none";
-    iconCopy[0].style.display = "inline-block";
+function cleanText() {
+  texto.value = "";
+  phrases.innerHTML = "";
+  result.innerHTML = "";
+  result.classList.remove("backResult");
+  btnClean.style.display = "none";
+  btnCopyText.innerText = "Copiar texto";
+  btnMain.style.background = "#e84393";
+  iconCheck[0].style.display = "none";
+  iconCopy[0].style.display = "inline-block";
 }
 
-function clean(){
-    cleanText();
+function clean() {
+  cleanText();
 }
 
-const inputHandler = function(e) {
+const inputHandler = function (e) {
   result.innerHTML = e.target.value;
   result.classList.add("backResult");
   btnClean.style.display = "inline-block";
-  if(texto.value == ''){
+  if (texto.value == "") {
     cleanText();
   }
-}
+};
 
-texto.addEventListener('input', inputHandler);
+texto.addEventListener("input", inputHandler);
